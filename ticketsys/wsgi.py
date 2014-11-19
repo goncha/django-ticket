11a97c7eca9ba243479486027e0a2bf7a5131801
Gang Chen <goncha@gmail.com>
2014-11-19 09:35:25 +0800

    Add backbone of project

---
+++ ticketsys/wsgi.py
@@ -0,0 +1,14 @@
+"""
+WSGI config for ticketsys project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/1.7/howto/deployment/wsgi/
+"""
+
+import os
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "ticketsys.settings")
+
+from django.core.wsgi import get_wsgi_application
+application = get_wsgi_application()
