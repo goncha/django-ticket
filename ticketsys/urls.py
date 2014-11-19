8726f8a7b19a91838b4869fb4f15226fb32252d4
Gang Chen <goncha@gmail.com>
2014-11-19 13:56:57 +0800

    Import urls of accounts app

--- ticketsys/urls.py
+++ ticketsys/urls.py
@@ -7,5 +7,5 @@ urlpatterns = patterns('',
     # url(r'^blog/', include('blog.urls')),
 
     url(r'^admin/', include(admin.site.urls)),
-    url(r'^accounts/', include('accounts.urls')),
+    url(r'^accounts/', include('accounts.urls', namespace='accounts')),
 )
